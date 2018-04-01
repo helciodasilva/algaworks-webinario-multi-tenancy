abe6b9d78dd2f5ecc7a8ad529c94a28a92c2cc09
Helcio da Silva <helciodasilva@gmail.com>
2018-04-01 10:16:15 -0300

    Exemplo 1

---
+++ src/main/java/com/algaworks/webinario/multi/tenancy/model/Funcionario.java
@@ -0,0 +1,84 @@
+package com.algaworks.webinario.multi.tenancy.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "funcionario")
+public class Funcionario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+
+	private String nome;
+
+	@Column(name = "tenant_id")
+	private String tenantId;
+
+	public Funcionario() {
+	}
+
+	public Funcionario(String nome) {
+		this.nome = nome;
+	}
+
+	public Funcionario(String nome, String tenantId) {
+		this(nome);
+		this.tenantId = tenantId;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTenantId() {
+		return tenantId;
+	}
+
+	public void setTenantId(String tenantId) {
+		this.tenantId = tenantId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Funcionario other = (Funcionario) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+
+}
